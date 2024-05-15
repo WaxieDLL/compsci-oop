c4efd9978fc23ca6d3fd3ebe308953649d469c3e
Waxie <43115019+WaxieDLL@users.noreply.github.com>
2024-05-15 16:47:35 +0200

    Set up raylib for 2d visualization of project

--- main.cpp
+++ main.cpp
@@ -1,12 +1,22 @@
-﻿// compsci-oop.cpp : Defines the entry point for the application.
-//
-
-#include "compsci-oop.h"
-
-using namespace std;
+﻿#include <raylib.h>
+#define WINDOW_WIDTH 800
+#define WINDOW_HEIGHT 600
+#define WINDOW_TITLE "OOP project"
+#define DEFAULT_TEXT "Hello, OOP project..."
+#define DEFAULT_TEXT_SIZE 30
 
 int main()
 {
-	cout << "Hello CMake." << endl;
+	InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_TITLE);
+	SetWindowState(FLAG_WINDOW_RESIZABLE);
+	SetTargetFPS(0);
+	while(!WindowShouldClose())
+	{
+		BeginDrawing();
+		ClearBackground(WHITE);
+		const Vector2 text_size = MeasureTextEx(GetFontDefault(), DEFAULT_TEXT, DEFAULT_TEXT_SIZE, DEFAULT_TEXT_SIZE / 10);
+		DrawText("Hello, OOP project...", GetScreenWidth() / 2 - static_cast<int>(text_size.x / 2), GetScreenHeight() / 2 - static_cast<int>(text_size.y / 2), DEFAULT_TEXT_SIZE, BLACK);
+		EndDrawing();
+	}
 	return 0;
 }
