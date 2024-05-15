50a5af18b761c597b9d15ca142c70e50a482bd90
Waxie <43115019+WaxieDLL@users.noreply.github.com>
2024-05-15 16:16:58 +0200

    Add project files.

---
+++ main.cpp
@@ -0,0 +1,12 @@
+﻿// compsci-oop.cpp : Defines the entry point for the application.
+//
+
+#include "compsci-oop.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
